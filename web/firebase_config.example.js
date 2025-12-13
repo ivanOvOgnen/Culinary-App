4536896eecc6573352d936e2f7467b23ee0e28d8
ivanOvOgnen <ognen.ivanov@gmail.com>
2025-12-13 17:34:24 +0100

    Env Examples

---
+++ web/firebase_config.example.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.firebasestorage.app",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:xxxxxxxxxxxxxxxxxxxx"
+};
